865848a03dfd6cea379347e4233ba93c45cf858d
ngthanh <thanh.nn1106@gmail.com>
2018-02-14 01:19:09 +0700

    Change menu top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
                     <ul class="nav navbar-nav navbar-left">
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                Lession 1 <span class="caret"></span>
+                                Chapters<span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu" role="menu">
                                 <li>
@@ -51,6 +51,11 @@
                                     </a>
                                 </li>
                             </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                Reviews
+                            </a>
                         </li>
                     </ul>
                     @endif
